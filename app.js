440b26eabe7c9dfa5e538efd8f10e7d6a9755ec3
Ash Winter <ash_winter@hotmail.co.uk>
2018-02-25 20:03:25 +0000

    Removed the files and code we wish attendees to add for exercise one

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 var express = require('express');
 var app = express();
-var index = require('./routes/index');
-
-//Routes to use
-app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
